994327ce806f4bdaa6361b7bdccb1a5149cdb5d6
petermat <p.matkovki@gmail.com>
2019-08-01 14:57:08 +0200

    secret key to local settings

--- project/settings.py
+++ project/settings.py
@@ -24,6 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'j(2!-w)2522aibi6c9*_g+c#+eyj9b_5q^cdy_q&fsms+hua=j'
 
 CSRF_COOKIE_HTTPONLY = True
+CSRF_COOKIE_SECURE = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
